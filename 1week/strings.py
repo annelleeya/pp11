784a0b5d33c319676d66439b322ad8d4e09278ed
annelleeya <a_kidraliyeva@kbtu.kz>
2024-01-22 09:49:36 +0600

    first commit

--- 1week/strings.py
+++ 1week/strings.py
@@ -45,3 +45,56 @@ print("expensive" not in txt)
 txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+
+#11
+b = "Hello, World!"
+print(b[2:5])
+
+#12
+b = "Hello, World!"
+print(b[:5])
+
+#13
+b = "Hello, World!"
+print(b[-5:-2])
+
+#14
+a = "Hello, World!"
+print(a.upper())
+
+#15
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#16
+a = "Hello, World!"
+print(a.split(","))
+
+#17
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#18
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#19
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+#20
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+#21
+txt = "We are the so-called \"Vikings\" from the north."
+
+#22
